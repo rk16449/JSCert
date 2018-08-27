9d6ab9461a372eb5ccc04a4c216220dfb087a65a
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-27 14:15:14 +0100

    completed palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -6,7 +6,22 @@ and turn everything into the same case (lower or upper case) in order to check f
 */
 
 function palindrome(str) {
-  return true;
+	
+	// Create a new string, remove non-alphanumeric and convert to lowercase
+	var newStr = str.replace(/[^0-9a-z]/gi, '')
+	newStr = newStr.toLowerCase();
+	
+	// Split the string into an array
+	var splitString = newStr.split("");
+	
+	// Reverse the array
+	var reverseArray = splitString.reverse();
+	
+	// Join all elements in array to a string
+	var joinArray = reverseArray.join("");
+	
+	// Check if it equals to newStr
+	return newStr === joinArray;
 }
 
 
