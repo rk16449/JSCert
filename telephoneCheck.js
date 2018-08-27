1216cf12c426ca8c7f9390e4b2e740ee7abe1cb8
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-27 18:26:48 +0100

    updated telephoneCheck.js, reading doc about regExp

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,13 +1,36 @@
 // Returns true if a valid usa phone number format was entered
 
+
+/*
+Valid inputs
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+1(555)555-5555 <-- biggest string length
+*/
+
 function telephoneCheck(str) {
 	
-	// The minimum length a string is 5555555555 long
+	// The minimum length a string is 5555555555 long and max is 1(555)555-5555
+	if(str.length < 10 && str.length > 14) return false;
 	
-	if(str.length < 10) return false;
+	// Now that we have enough string values do a regEx check
+	
+	// Check that there is a 1 or there isn'tags
+	
+	// Check for 3 digits either surrounded by brackets or a dash or a space
+	
+	// same again
+	
+	// check for 4 digits
 	
 	
-  return true;
+	
+	return true;
 }
 
 telephoneCheck("555-555-5555");
